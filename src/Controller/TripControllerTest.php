15e65211b099093d4af3332896331623ccd6f4df
VeronSheva <veron.sheva@gmail.com>
2022-10-13 01:22:24 +0300

    changed route in TripControllerTest

--- src/Controller/TripControllerTest.php
+++ src/Controller/TripControllerTest.php
@@ -10,7 +10,7 @@ class TripControllerTest extends WebTestCase
     public function testGetTrips(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/trips');
+        $client->request('GET', '/api/v1/trips');
         $responseContent = $client->getResponse()->getContent();
 
         $this->assertResponseIsSuccessful();
